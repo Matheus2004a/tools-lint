dc3284014dd4223e93786952ae5f985c442a4772
Matheus Aurélio <matheusaurelio2004@gmail.com>
2023-10-14 13:29:42 -0300

    chore: setup configs lints

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+import './App.css';
+import reactLogo from './assets/react.svg';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button
+          type="button"
+          onClick={() => setCount((prevState) => prevState + 1)}
+        >
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
